c4b8a453466afb20f4881b18eb1f9a60abf24c1d
Jahidul Islam <jahidul.islam114593@gmail.com>
2024-09-08 23:13:55 +0600

    [secure fireBase.init.js]

--- src/components/FireBase/fireBase.init.js
+++ src/components/FireBase/fireBase.init.js
@@ -1,17 +1,14 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
 // Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyBYxEi3G3E6DRPcvQLH1XHYArsE3R49Ykw",
-  authDomain: "fir-2-react-auth-d1b25.firebaseapp.com",
-  projectId: "fir-2-react-auth-d1b25",
-  storageBucket: "fir-2-react-auth-d1b25.appspot.com",
-  messagingSenderId: "762652686389",
-  appId: "1:762652686389:web:8456a87182439c4af2997c",
+  apiKey: import.meta.env.VITE_apiKey,
+  authDomain: import.meta.env.VITE_authDomain,
+  projectId: import.meta.env.VITE_projectId,
+  storageBucket: import.meta.env.VITE_storageBucket,
+  messagingSenderId: import.meta.env.VITE_messagingSenderId,
+  appId: import.meta.env.VITE_appId,
 };
 
 // Initialize Firebase
--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -39,9 +39,12 @@ const Navbar = () => {
             <li>
               <NavLink to={"/register"}>Register</NavLink>
             </li>
+            <li>
+              <Link to="/about">About</Link>
+            </li>
           </ul>
         </div>
-        <a className="btn btn-ghost text-xl">daisyUI</a>
+        <a className="btn btn-ghost text-xl">FireBase-Auth</a>
       </div>
       <div className="navbar-center hidden lg:flex">
         <ul className="menu menu-horizontal px-1">
